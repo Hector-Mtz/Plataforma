dc3e82b57e40cf7a53a10560edba2a9ca6b2b536
Hector <hugo201123@gmail.com>
2023-01-18 22:16:14 -0600

    carrusel

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,21 +1,16 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Carrusel from  '../Pages/DashboardPartials/Carrusel.vue';
 
 </script>
 
 <template>
     <AppLayout title="Inicio">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Inicio
-            </h2>
-        </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                  
-                </div>
+               <div>
+                  <Carrusel></Carrusel>
+               </div>
             </div>
         </div>
     </AppLayout>
---
+++ resources/js/Pages/DashboardPartials/Carrusel.vue
@@ -0,0 +1,54 @@
+<script>
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade  } from 'swiper'; 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        //console.log(swiper);
+      };
+      const onSlideChange = () => {
+       // console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, A11y, Autoplay, EffectFade],
+        EffectFade
+      };
+    },
+  };
+</script>
+<template>
+  <swiper
+   :modules="modules"
+    effect="fade"
+    :slides-per-view="1"
+    :space-between="50"
+     navigation
+	 :autoplay='{
+               "delay": 3000,
+              "disableOnInteraction": false
+     }'
+    :pagination="{ clickable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>
+        <img class="w-screen" src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2017/04/marvel-future-fight_9.jpg?itok=AvLMbUwV">
+    </swiper-slide>
+    <swiper-slide>
+        <img class="w-screen" src="https://i.ytimg.com/vi/-LTuziS6hiQ/maxresdefault.jpg">
+    </swiper-slide>
+  </swiper>
+</template>
